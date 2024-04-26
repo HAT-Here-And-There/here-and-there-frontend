4b2d3b3175dd5c546017fb0e3591294c7b5951f9
Programming-Seungwan <swsj4480@gmail.com>
2024-04-26 18:07:34 +0900

    Style : 다른 모듈을 import 시의 상대 경로를 절대 경로로 변환
    1. vite.config.ts 파일과 ts.config.json 파일의 수정
    2. @디렉터리명을 통해 절대경로 import 문법을 쓰면 됨

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { store } from './context/store.tsx';
+import { store } from '@context/store.tsx';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@context', replacement: '/src/context' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@types', replacement: '/src/types' },
+      { find: '@utils', replacement: '/src/utils' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
+});
