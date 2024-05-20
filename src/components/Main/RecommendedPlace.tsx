b3025509f9a99b614b6f996ccd412596b0ce800f
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-05-20 19:12:27 +0900

    Feat : 메인페이지 컴포넌트 작업중

--- src/components/Main/RecommendedPlace.tsx
+++ src/components/Main/RecommendedPlace.tsx
@@ -1,22 +1,57 @@
 import { useState, useEffect } from 'react';
 
+interface Place {
+  id: number;
+  name: string;
+  imageUrl: string;
+  description: string;
+}
+
 export default function RecommendedPlace() {
-  const [recommendedPlaces, setRecommendedPlaces] = useState<null | object>(
-    null
-  );
+  const [recommendedPlaces, setRecommendedPlaces] = useState<Place[]>([]);
 
   useEffect(() => {
     async function getData() {
       const response = await fetch('http://172.233.70.162/tour/places?size=8');
-
       const result = await response.json();
-
       setRecommendedPlaces(result.places);
     }
 
     getData();
   }, []);
 
-  console.log(recommendedPlaces);
-  return <div className="container mx-auto flex items-center"></div>;
+  return (
+    <PlaceComponent places={recommendedPlaces} />
+  );
+}
+
+interface PlaceComponentProps {
+  places: Place[];
+}
+
+function PlaceComponent({ places }: PlaceComponentProps) {
+  return (
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-2xl grid grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-1 lg:px-8">
+        <div>
+          <h2 className="text-2xl font-custom text-center tracking-tight text-gray-900 sm:text-4xl">
+            여기저기 여행을 시작해보세요!
+          </h2>
+          <p className="mt-4 font-custom text-center text-gray-500">
+            지금 대한민국에서 <span className="text-red-300">인기 많은 여행지</span>는 어디일까요?
+          </p>
+        </div>
+        <div className="grid grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
+          {places.map((place) => (
+            <img
+              key={place.id}
+              src={place.imageUrl}
+              alt={place.name}
+              className="rounded-lg bg-gray-100"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
