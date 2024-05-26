f1b64454b766d98242f61d321b1d67ece908db41
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-05-26 18:16:37 +0900

    Fix : 장소 컴포넌트 규격 오류 수정

--- src/components/Main/PlaceComponent.tsx
+++ src/components/Main/PlaceComponent.tsx
@@ -7,23 +7,24 @@ export default function PlaceComponent({ places }: PlaceComponentProps) {
   return (
     <div className="grow flex flex-col px-80 justify-center bg-dftBackgroundGray">
       <div>
-        <h2 className="text-2xl font-main text-center tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-4xl font-main text-center tracking-tight text-gray-900">
           여기저기 여행을 시작해보세요!
         </h2>
         <p className="mt-4 mb-4 font-main text-center text-gray-500">
           지금 대한민국에서{' '}
-          <span className="text-red-300 ">인기 많은 여행지</span>는 어디일까요?
+          <span className="text-red-300">인기 많은 여행지</span>는 어디일까요?
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
+      <div className="grid grid-cols-4 gap-6">
         {places.map((place) => (
-          <img
-            key={place.id}
-            src={place.imageUrl}
-            alt={place.name}
-            className="rounded-lg bg-gray-100 hover:cursor-pointer"
-            onClick={() => navigate(`/chatroom/${place.id}`)}
-          />
+          <div key={place.id} className="rounded-lg bg-gray-100 overflow-hidden hover:cursor-pointer">
+            <img
+              src={place.imageUrl}
+              alt={place.name}
+              className="object-cover h-full"
+              onClick={() => navigate(`/chatroom/${place.id}`)}
+            />
+          </div>
         ))}
       </div>
     </div>
