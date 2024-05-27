e78a3070a030580974feb5238b843e80270db7b7
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-05-27 21:59:53 +0900

    Fix : HAT-111

--- src/components/all/Footer.tsx
+++ src/components/all/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="h-20">
-      <footer className="flex justify-center bg-dftBackgroundGray">
+      <footer className="h-20 flex justify-center items-center bg-dftBackgroundGray">
         <img
           src="/assets/EnglishLogo.svg"
           alt="This is english here and there logo!"
