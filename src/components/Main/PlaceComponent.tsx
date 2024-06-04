d4c50c0d031d60357c5158f60e5b34dc69a59d79
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-06-04 14:44:27 +0900

    Chore : 메인페이지 장소UI와 지역리스트 UI css 수정

--- src/components/Main/PlaceComponent.tsx
+++ src/components/Main/PlaceComponent.tsx
@@ -6,8 +6,8 @@ export default function PlaceComponent({ places }: PlaceComponentProps) {
 
   return (
     <div className="flex flex-col items-center w-full h-[1000px] pt-16 pb-32  bg-dftBackgroundGray">
-      <div className="mb-[73px]">
-        <h2 className="text-3xl font-main text-center tracking-tight text-gray-900">
+      <div className="mb-16">
+        <h2 className="text-4xl font-main text-center tracking-tight text-gray-900">
           여기저기 여행을 시작해보세요!
         </h2>
         <p className="mt-4 mb-4 font-main text-center text-gray-500">
@@ -15,17 +15,17 @@ export default function PlaceComponent({ places }: PlaceComponentProps) {
           <span className="text-red-300">인기 많은 여행지</span>는 어디일까요?
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-4 w-[46%] h-[58%]">
+      <div className="grid grid-cols-4 gap-4">
         {places.map((place, index) => (
           <div
             key={place.id}
-            className="relative pb-[100%] w-[130px] h-[40px] overflow-hidden rounded-lg bg-gray-100 hover:cursor-pointer"
+            className="relative pb-[100%] w-[250px] h-[20px] overflow-hidden rounded-lg bg-gray-100 hover:cursor-pointer"
             onClick={() => navigate(`/chatroom/${place.id}`)}
           >
             <img
               src={place.imageUrl}
               alt={place.name}
-              className="absolute top-0 left-0 w-full h-5/6 object-cover"
+              className="absolute top-0 left-0 w-full h-full object-cover"
             />
             <div className="absolute bottom-0 left-0 w-full bg-gray-800 text-white text-center py-2">
               Top. {index + 1}
