6a45a4d6711109dab265beda63045cb1081e5319
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-08-14 15:53:07 +0900

    Feat : 장소 추가 버튼 퍼블리싱

--- src/components/travel-plan-all/overall/OverallRightSectionOneDayItem.tsx
+++ src/components/travel-plan-all/overall/OverallRightSectionOneDayItem.tsx
@@ -17,19 +17,31 @@ export default function OverallRightSectionOneDayItem({
 }: overallRightSectionOneDayItemProps) {
   return (
     <div className="w-fit min-w-[500px] h-auto flex flex-col pt-5 pl-10 border-t-[5px] border-r-[1px] border-b-[5px] border-l-[1px] border-black rounded-lg bg-white overflow-y-auto">
-      <div className="mb-6 text-black text-xl font-main">Day {dayIndex + 1}</div>
+      <div className="mb-6 text-black text-xl font-main">
+        Day {dayIndex + 1}
+      </div>
       <div className="relative w-full flex-grow">
         <div className="absolute left-4 top-0 bottom-0 w-[1px] bg-black"></div>
         {placeList.map((placeListItem, placeIndex) => (
-          <div key={placeListItem.id} className="flex w-full relative mb-12 items-start">
-            <div className="absolute left-3 top-0 flex items-center">
-              <div className="w-3 h-3 rounded-full bg-planListRed"></div>
+          <div
+            key={placeListItem.id}
+            className="flex w-full relative mb-12 items-start"
+          >
+            {/* 장소 추가 버튼 퍼블리싱 */}
+            <div className="absolute left-1 flex items-center">
+              <img
+                src="/assets/Add-icon.svg"
+                alt="장소 추가"
+                className="w-6 h-6"
+              />
             </div>
             {/* 장소 정보 및 장소 순서 변경 버튼 */}
             <div className="flex-grow flex flex-col ml-10">
               <div className="flex justify-between items-center">
                 <div className="flex items-center">
-                  <h2 className="text-lg font-bold font-main ml-14">{placeListItem.name}</h2>
+                  <h2 className="text-lg font-bold font-main ml-14">
+                    {placeListItem.name}
+                  </h2>
                 </div>
                 <div className="flex mr-10">
                   <button
@@ -46,8 +58,15 @@ export default function OverallRightSectionOneDayItem({
                   >
                     <img src="/assets/DOWN.svg" alt="아래로" />
                   </button>
-                  <button onClick={() => onDelete(dayIndex, placeIndex)} className="px-5">
-                    <img src="/assets/delete-icon.svg" alt="삭제" className="w-6 h-6" />
+                  <button
+                    onClick={() => onDelete(dayIndex, placeIndex)}
+                    className="px-5"
+                  >
+                    <img
+                      src="/assets/delete-icon.svg"
+                      alt="삭제"
+                      className="w-6 h-6"
+                    />
                   </button>
                 </div>
               </div>
