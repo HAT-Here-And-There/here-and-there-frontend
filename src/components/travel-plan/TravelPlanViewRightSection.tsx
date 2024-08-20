55fc1c40af55c31a14d4e7c39eab56f93e18bd01
Programming-Seungwan <swsj4480@gmail.com>
2024-08-20 15:09:18 +0900

    Fix : /travel-plan 페이지 수정

--- src/components/travel-plan/TravelPlanViewRightSection.tsx
+++ src/components/travel-plan/TravelPlanViewRightSection.tsx
@@ -7,17 +7,17 @@ interface travelPlanViewRightSectionProps {
 export default function TravelPlanViewRightSection({
   dailyPlans,
 }: travelPlanViewRightSectionProps) {
-  console.log(dailyPlans);
   return (
     <div className="w-[65%] h-full travel-plan-scroll-box flex font-main overflow-x-auto">
-      {dailyPlans.map((planElement, index) => {
+      {dailyPlans.map((planElement) => {
         return (
-          <div className="border-[1px] border-black min-w-[500px] flex flex-col items-center gap-y-5">
-            <div>Day {planElement.dayNumber}</div>
+          <div className="border-x-[1px] border-y-[5px] rounded-lg overflow-y-scroll border-black min-w-[500px] flex flex-col items-center gap-y-5 pb-10">
+            <div className="w-[65%] mt-[35px] flex justify-start items-center">
+              Day {planElement.dayNumber}
+            </div>
             {planElement.dailyPlanItems.map((element) => {
-              console.log(element);
               return (
-                <div className="w-fit flex flex-col items-center gap-y-3">
+                <div className="w-fit h-fit flex flex-col items-center gap-y-3">
                   <p className="w-full flex justify-start">
                     {element.place.name}
                   </p>
@@ -26,7 +26,12 @@ export default function TravelPlanViewRightSection({
                     alt="This is place image"
                     className="w-[305px] h-[132px] rounded-xl object-cover"
                   />
-                  <input type="text" placeholder={element.memo} />
+                  <div className="mt-2 flex justify-center">
+                    <textarea
+                      className="w-[305px] h-[50px] rounded-xl border border-gray-300 p-2"
+                      placeholder={element.memo}
+                    ></textarea>
+                  </div>
                 </div>
               );
             })}
