ab414a3c8471bde448e46f7db8cb78e85219e27a
Programming-Seungwan <swsj4480@gmail.com>
2024-08-21 17:48:22 +0900

    Feat : 날짜 순서 조정, 삭제 기능 1차 구현

--- src/components/travel-plan/TravelPlanViewRightSection.tsx
+++ src/components/travel-plan/TravelPlanViewRightSection.tsx
@@ -10,13 +10,75 @@ export default function TravelPlanViewRightSection({
   dailyPlans,
   handleTravelPlanData,
 }: travelPlanViewRightSectionProps) {
-  const modifyPlanState = (
+  console.log(dailyPlans);
+  const modifyPlanFunction = (
     dayIndex: number,
     placeOrderIndex: number,
-    actionType: 'up' | 'down' | 'delete'
+    type: 'up' | 'down' | 'delete'
   ): void => {
-    handleTravelPlanData((prevData: travelPlanDataProp | null) => {
-      return prevData;
+    handleTravelPlanData((prevData) => {
+      const newDailyPlansArray: dailyPlansProps[] = [];
+      for (let i = 0; i < dailyPlans.length; i++) {
+        if (i !== dayIndex) {
+          newDailyPlansArray.push(dailyPlans[i]);
+        } else {
+          if (type === 'up') {
+            if (placeOrderIndex === 0) {
+              newDailyPlansArray.push(dailyPlans[i]);
+            } else {
+              const temp = JSON.parse(
+                JSON.stringify(dailyPlans[i].dailyPlanItems[placeOrderIndex])
+              );
+              dailyPlans[i].dailyPlanItems[placeOrderIndex] =
+                dailyPlans[i].dailyPlanItems[placeOrderIndex - 1];
+              dailyPlans[i].dailyPlanItems[placeOrderIndex - 1] = temp;
+              newDailyPlansArray.push({
+                date: dailyPlans[i].date,
+                dayNumber: dailyPlans[i].dayNumber,
+                id: dailyPlans[i].id,
+                dailyPlanItems: dailyPlans[i].dailyPlanItems,
+              });
+            }
+          } else if (type === 'down') {
+            if (
+              placeOrderIndex ===
+              dailyPlans[placeOrderIndex].dailyPlanItems.length - 1
+            ) {
+              newDailyPlansArray.push(dailyPlans[i]);
+            } else {
+              const temp = JSON.parse(
+                JSON.stringify(dailyPlans[i].dailyPlanItems[placeOrderIndex])
+              );
+              dailyPlans[i].dailyPlanItems[placeOrderIndex] =
+                dailyPlans[i].dailyPlanItems[placeOrderIndex + 1];
+              dailyPlans[i].dailyPlanItems[placeOrderIndex + 1] = temp;
+              newDailyPlansArray.push({
+                date: dailyPlans[i].date,
+                dayNumber: dailyPlans[i].dayNumber,
+                id: dailyPlans[i].id,
+                dailyPlanItems: dailyPlans[i].dailyPlanItems,
+              });
+            }
+          } else if (type === 'delete') {
+            const tmpDailyPlanItems = dailyPlans[i].dailyPlanItems.filter(
+              (element, i) => i !== placeOrderIndex
+            );
+            newDailyPlansArray.push({
+              date: dailyPlans[i].date,
+              dayNumber: dailyPlans[i].dayNumber,
+              id: dailyPlans[i].id,
+              dailyPlanItems: tmpDailyPlanItems,
+            });
+          }
+        }
+      }
+      return {
+        id: prevData?.id as number,
+        name: prevData?.name as string,
+        startDate: prevData?.startDate as string,
+        endDate: prevData?.endDate as string,
+        dailyPlans: newDailyPlansArray,
+      };
     });
   };
 
@@ -27,20 +89,39 @@ export default function TravelPlanViewRightSection({
           <div
             className="border-x-[1px] border-y-[5px] rounded-lg overflow-y-scroll border-black min-w-[500px] flex flex-col items-center gap-y-5 pb-10"
             key={planElement.id}
-            onClick={() => {}}
           >
             <div className="w-[65%] mt-[35px] flex justify-start items-center">
               Day {planElement.dayNumber}
             </div>
-            {planElement.dailyPlanItems.map((element) => {
+            {planElement.dailyPlanItems.map((element, index) => {
               return (
                 <div
                   className="w-fit h-fit flex flex-col items-center gap-y-3"
                   key={element.id}
                 >
-                  <p className="w-full flex justify-start">
+                  <div className="w-full flex justify-between items-center">
                     {element.place.name}
-                  </p>
+                    <div className="flex w-fit justify-around items-center gap-x-2">
+                      <img
+                        src="/assets/UP.svg"
+                        alt="This is up svg image file"
+                        className="hover:cursor-pointer"
+                        onClick={() => modifyPlanFunction(idx, index, 'up')}
+                      />
+                      <img
+                        src="/assets/DOWN.svg"
+                        alt="This is down svg image file"
+                        className="hover:cursor-pointer"
+                        onClick={() => modifyPlanFunction(idx, index, 'down')}
+                      />
+                      <img
+                        src="/assets/delete-icon.svg"
+                        alt="This is delete svg image file"
+                        className="w-6 h-6 hover:cursor-pointer"
+                        onClick={() => modifyPlanFunction(idx, index, 'delete')}
+                      />
+                    </div>
+                  </div>
                   <img
                     src={element.place.imageUrl}
                     alt="This is place image"
